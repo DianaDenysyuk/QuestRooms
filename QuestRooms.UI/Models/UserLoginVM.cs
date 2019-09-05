751a2aaaded9161e744ae91391f843be888817af
Діана Денисюк <didenysyuk@gmail.com>
2019-09-05 11:41:13 +0300

    modificate registration + login (need add admin panel)

--- QuestRooms.UI/Models/UserLoginVM.cs
+++ QuestRooms.UI/Models/UserLoginVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,14 @@ namespace QuestRooms.UI.Models
 {
     public class UserLoginVM
     {
+        [Required(ErrorMessage = "Field is required")]
+        [EmailAddress(ErrorMessage = "Wrong format")]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email")]
         public string Login { get; set; }
+        [Required(ErrorMessage = "Field is required")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
         public string Password { get; set; }
     }
 }
--- QuestRooms.UI/Models/UserRegisterVM.cs
+++ QuestRooms.UI/Models/UserRegisterVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,22 @@ namespace QuestRooms.UI.Models
 {
     public class UserRegisterVM
     {
+        [Required(ErrorMessage = "Field is required")]
+        [EmailAddress(ErrorMessage = "Wrong format")]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email")]
         public string Login { get; set; }
+
+        [Required(ErrorMessage = "Field is required")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "Field is required")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "Password must match")]
+        public string ConfirmPassword { get; set; }
 
     }
 }
